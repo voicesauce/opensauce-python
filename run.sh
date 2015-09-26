ce322690ede003ba497c0d704b50ed5043f9991c
k8si <ksilvers@cs.umass.edu>
2015-09-26 12:28:13 -0400

    comments

--- run.sh
+++ run.sh
@@ -5,11 +5,19 @@ then
     echo "set environment variable VS_ROOT first!"
 else
     root=$VS_ROOT
+
+    # path to an input directory of *.wav files
     inputDir="$root/defaults/sounds"
+    
+    # path to the directory where you want to store OpenSauce output
     outputDir="$root/output"
-    mkdir -pv $outputDir
+    mkdir -pv $outputDir # if the output dir already doesn't exist, create it
+
+    # path to your settings file
     settings="$root/defaults/settings/default.csv"
+    
+    # path to the list of measurements you want to extract from the input
     params="$root/defaults/parameters/default.csv"
-    echo "input: $inputDir, output: $outputDir, settings: $settings, params: $params"
+
     python $root/opensauce/process.py -i $inputDir -o $outputDir -s $settings -p $params
 fi
