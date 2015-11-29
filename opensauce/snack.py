98666fa7a283dd63170159fb9afbddcdf17000b4
R. David Murray <rdmurray@bitdance.com>
2015-11-29 17:16:32 -0500

    Add better snack function, and a (failing) test.
    I can't see what is wrong with my translation of the VoiceSauce code,
    so I'm committing this so others can run it and see if the problem is
    that snack doesn't always produce the same output (I'm thinking it
    may be different in different versions of snack).

---
+++ opensauce/snack.py
@@ -0,0 +1,60 @@
+"""Interface to the 'snack' audio library.
+
+Requires that snack be installed where the tcl/tk used by Python can find it.
+On linux systems this normally means installing it via the system package
+manager.
+
+"""
+
+# XXX We may need to add a call to snack.exe on Windows if a snack install on
+# Windows isn't visible to Windows Python.
+
+import logging
+log = logging.getLogger('opensauce.snack')
+
+try:
+    import tkinter
+except ImportError:
+    import Tkinter as tkinter
+
+def snack_pitch(wav_fn, frame_length=None, window_length=None, max_pitch=None,
+                min_pitch=None):
+    """Return F0 and voicing vectors computed from the data in wav_fn.
+
+    Use the snack esps method and extract the pitch (F0) and voicing values for
+    each frame.
+
+    windows_size and frame_shift are in seconds, max_pitch and min_pitch in
+    Hertz.  Defaults are the snack defaults.
+
+    """
+    # XXX I'm assuming Hz for pitch; the docs don't actually say that.
+    # http://www.speech.kth.se/snack/man/snack2.2/tcl-man.html#spitch
+    tcl = tkinter.Tcl()
+    try:
+        # XXX This will trigger a message 'cannot open /dev/mixer' on the
+        # console if you don't have a /dev/mixer.  You don't *need* a mixer to
+        # snack the way we are using it, but there's no practical way to
+        # suppress the message without modifying the snack source.  Fortunately
+        # most people running opensauce will in fact have a /dev/mixer.
+        tcl.eval('package require snack')
+    except tkinter.TclError as err:
+        log.critical('Cannot load snack (is it installed?): %s', err)
+        return
+    tcl.eval('snack::sound s')
+    tcl.eval('s read {}'.format(wav_fn))
+    cmd = ['s pitch -method esps']
+    local_vars = locals()
+    # Let snack use its defaults if no value specified for the keywords.
+    for v in ('frame_length', 'window_length', 'max_pitch', 'min_pitch'):
+        if local_vars[v] is not None:
+            cmd.extend(['-' + v.replace('_', '') + ' ' + str(local_vars[v])])
+    tcl.eval('set data [{}]'.format(' '.join(cmd)))
+    num_frames = int(tcl.eval('llength $data'))
+    F0, V = [], []
+    # snack returns four values per frame, we only care about the first two.
+    for i in range(num_frames):
+        values = tcl.eval('lindex $data ' + str(i)).split()
+        F0.append(float(values[0]))
+        V.append(float(values[1]))
+    return F0, V
---
+++ test/test_snack.py
@@ -0,0 +1,45 @@
+from __future__ import division
+
+import math
+
+from opensauce.snack import snack_pitch
+
+from support import TestCase, wav_fns, get_test_data
+
+class TestSnack(TestCase):
+
+    def test_defaults(self):
+        # XXX I think these are the voicesauce defaults (vs expresses them
+        # in ms, but snack expects seconds).
+        f_len = 0.001
+        w_len = 0.025
+        for fn in wav_fns:
+            # XXX I think voicesauce will optionally emit the Voice data, but I
+            # don't have example output data for it, so I'm ignoring it for now.
+            F0, V = snack_pitch(fn, frame_length=f_len, window_length=w_len)
+            # The first samples in all of our test data yield 0.
+            self.assertEqual(F0[:10], [0.0]*10)
+            # The snack esps data starts in the middle of the first window,
+            # but the voicesauce output data starts counting frames from 0,
+            # so pad our F0 with invalid frames.
+            # XXX I'm not sure why -1 seems to make the data match up better,
+            # voicesauce doesn't have the -1 term.
+            F0[:0] = [float('NaN')] * int(math.floor(w_len/f_len/2)-1)
+            # NB: It doesn't matter which output file we use, the sF0 column is
+            # the same in all of them.
+            voicesauce_data = get_test_data(fn, 'sF0', 'sf0', '1ms')
+            os_data = []
+            vs_data = []
+            for ms, fs0 in voicesauce_data:
+                ms = int(ms)
+                # Voicesauce rounds to three places.
+                os_data.append(round(F0[ms], 3))
+                vs_data.append(fs0)
+            # XXX debug prints.
+            #for i in range(len(os_data)):
+            #    print((i, os_data[i], voicesauce_data[i]))
+            for i in range(len(os_data)):
+                if (abs(os_data[i] - vs_data[i]) >= 40
+                       and (os_data[i]==0 or vs_data[i]==0)):
+                    continue
+                self.assertLess(abs(os_data[i] - vs_data[i]), 0.6)
