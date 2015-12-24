6e66cbfa43864339443850ec0bde701454a1c716
R. David Murray <rdmurray@bitdance.com>
2015-12-24 13:09:18 -0500

    Use scipy.io.loadmat, and actually commit the data.
    I forgot to commit the sample data in the first commit.  The actual
    checked in data here is in matlab binary format, so we can load it using
    scip.io.loadmat.  That allows us to to put all the expected data in the
    data file.  I've also had second thoughts about whether the floats
    will compare equal on all platforms, so I've changed the doc note
    to explain how to fix it if it turns out they don't.

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,3 +1,6 @@
+import numpy as np
+from scipy.io import loadmat
+
 from opensauce.helpers import wavread
 
 from test.support import TestCase, data_file_path
@@ -6,18 +9,11 @@ class TestSupport(TestCase):
 
     def test_wavfile(self):
         fn = data_file_path('beijing_f3_50_a.wav')
-        os_samples, Fs = wavread(fn)
-        with open(data_file_path('beijing_f3_50_a-y.mat')) as f:
-            lines = iter(f)
-            line = next(lines)
-            while line.strip().startswith('#'):
-                line = next(lines)
-            ml_samples = [float(line.strip())]
-            for line in lines:
-                line = line.strip()
-                if line:
-                    ml_samples.append(float(line))
-        self.assertEqual(Fs, 22050)   # agrees with octave value
-        # Both Python and Matlab/octave implement IEEE 754, so this should be
-        # an exact match.
-        self.assertEqual(list(os_samples), ml_samples)
+        samples, Fs = wavread(fn)
+        expected = loadmat(data_file_path('beijing_f3_50_a-wavfile-expected.mat'),
+                           squeeze_me=True)
+        self.assertEqual(Fs, expected['Fs'])
+        # XXX may need to use allclose here instead of array_equal.
+        if not np.array_equal(samples, expected['y']):
+            # Produce a useful error message for debugging.
+            self.assertEqual(list(samples), list(expected['y']))
