8fb02f0974b44e5aa5a1abaccd60b153aeddb01c
R. David Murray <rdmurray@bitdance.com>
2016-05-24 15:50:49 -0400

    Demote 'process' to "old cli", simplify test.
    Now that I've added TestCase.tmpfile the existing cli test case can be
    slightly simplified.

--- test/test_cli.py
+++ test/test_cli.py
@@ -3,27 +3,27 @@ import os
 from shutil import copytree
 from subprocess import Popen, PIPE
 
-from test.support import TestCase, TemporaryDirectory, data_file_path
+from test.support import TestCase, data_file_path
 
-class TestCLI(TestCase):
+class TestOldCLI(TestCase):
 
     def test_default_setup(self):
-        with TemporaryDirectory() as tmp:
-            def d(fn):
-                return os.path.join(tmp, fn)
-            os.mkdir(d('output'))
-            copytree('defaults', d('defaults'))
-            p = Popen(['python', 'opensauce/process.py',
-                            '-i', d('defaults/sounds'),
-                            '-o', d('output'),
-                            '-s', d('defaults/settings/default.csv'),
-                            '-p', d('defaults/parameters/default.csv'),
-                            ],
-                        stdout=PIPE,
-                        )
-            # For now, just ignore the output.
-            p.stdout.read()
-            rc = p.wait()
-            self.assertEqual(rc, 0)
-            self.assertTrue(filecmp.cmp(d('defaults/sounds/cant_c5_19a.f0'),
-                                        data_file_path('cant_c5_19a.f0')))
+        tmp = self.tmpdir()
+        def d(fn):
+            return os.path.join(tmp, fn)
+        os.mkdir(d('output'))
+        copytree('defaults', d('defaults'))
+        p = Popen(['python', 'opensauce/process.py',
+                        '-i', d('defaults/sounds'),
+                        '-o', d('output'),
+                        '-s', d('defaults/settings/default.csv'),
+                        '-p', d('defaults/parameters/default.csv'),
+                        ],
+                    stdout=PIPE,
+                    )
+        # For now, just ignore the output.
+        p.stdout.read()
+        rc = p.wait()
+        self.assertEqual(rc, 0)
+        self.assertTrue(filecmp.cmp(d('defaults/sounds/cant_c5_19a.f0'),
+                                    data_file_path('cant_c5_19a.f0')))
