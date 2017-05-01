978b6b46bc1f6b7cb70c14fd929e757a41436f87
Terri Yu <terri@terriyu.info>
2017-05-01 15:18:52 -0400

    Add assertion for checking arrays read from wavread

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -12,6 +12,7 @@ class TestSupport(TestCase):
         samples, Fs = wavread(fn)
         expected = loadmat('beijing_f3_50_a-wavread-expected')
         self.assertEqual(Fs, expected['Fs'])
+        self.assertTrue(np.array_equal(samples, expected['y']))
         # XXX may need to use allclose here instead of array_equal.
         if not np.array_equal(samples, expected['y']):
             # Produce a useful error message for debugging.
