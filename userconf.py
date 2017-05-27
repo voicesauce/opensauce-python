204f195891fa8f071009f0015fb81c280c3c5cbe
Terri Yu <terri@terriyu.info>
2017-05-27 01:51:46 -0400

    Add config file for global user-defined variables

---
+++ userconf.py
@@ -0,0 +1,8 @@
+# Additional configuration
+
+# User defined global variables
+
+# Snack calling method
+# Choices are 'exe', 'python', 'tcl'
+# Setting it to None, uses the default for your operating system
+user_default_snack_method = None
