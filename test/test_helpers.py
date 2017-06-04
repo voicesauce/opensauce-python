a693698dd0e61f2355b2f9f99a154c323cb65a87
Terri Yu <terri@terriyu.info>
2017-06-04 19:17:41 -0400

    Add tests for rounding and removing extra newlines

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,6 +1,8 @@
+import os
+import shutil
 import numpy as np
 
-from opensauce.helpers import wavread
+from opensauce.helpers import wavread, round_half_away_from_zero, remove_empty_lines_from_file
 
 from test.support import TestCase, data_file_path, load_json
 
@@ -13,3 +15,33 @@ class TestSupport(TestCase):
         expected = load_json('beijing_f3_50_a-wavread-expected')
         self.assertEqual(Fs, expected['Fs'])
         self.assertTrue(np.allclose(samples, expected['y']))
+
+    def test_round_half_away_from_zero(self):
+        self.assertEqual(round_half_away_from_zero(3.5), 4)
+        self.assertEqual(round_half_away_from_zero(3.2), 3)
+        self.assertEqual(round_half_away_from_zero(-2.7), -3)
+        self.assertEqual(round_half_away_from_zero(-4.3), -4)
+
+    def test_remove_empty_lines_from_file(self):
+        # Copy test file and remove extra newlines from it
+        fn = 'extra_newlines.txt'
+        t = self.tmpdir()
+        tmp_path = os.path.join(t, fn)
+        shutil.copy(data_file_path(fn), tmp_path)
+        remove_empty_lines_from_file(tmp_path)
+        # Read lines from original test file
+        with open(data_file_path(fn)) as f:
+            lines_orig = f.readlines()
+        # Read lines from copy with extra newlines removed
+        with open(tmp_path) as f:
+            lines_rm = f.readlines()
+        # Original file should be longer than copy with extra newslines removed
+        self.assertTrue(len(lines_orig) > len(lines_rm))
+        # Original and copy should match except for the removed newlines
+        idx = 0
+        for line_orig in lines_orig:
+            if line_orig.rstrip():
+                self.assertEqual(line_orig, lines_rm[idx])
+                idx += 1
+        # Copy should not contain any extra information
+        self.assertEqual(idx, len(lines_rm))
