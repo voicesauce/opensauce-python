ac14cb8134054b75491006c7986951f34d74f73d
Terri Yu <terri@terriyu.info>
2017-06-04 17:56:14 -0400

    Fix bug in test_helpers.py test
    * assertTrue statement needs to go after if statement

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -12,8 +12,8 @@ class TestSupport(TestCase):
         samples, Fs = wavread(fn)
         expected = loadmat('beijing_f3_50_a-wavread-expected')
         self.assertEqual(Fs, expected['Fs'])
-        self.assertTrue(np.array_equal(samples, expected['y']))
         # XXX may need to use allclose here instead of array_equal.
         if not np.array_equal(samples, expected['y']):
             # Produce a useful error message for debugging.
             self.assertEqual(list(samples), list(expected['y']))
+        self.assertTrue(np.array_equal(samples, expected['y']))
