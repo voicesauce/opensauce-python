6cf233f504f3273b25ec482c82bdb1accd9d3db8
Terri Yu <terri@terriyu.info>
2017-06-16 16:58:37 -0400

    Implement Snack formant algorithm, add tests, generate samples
    * Add sample files for Snack formants
    * Generate Snack formant samples for testing
    * Minor refactor in snack.py and test_snack.py
    * Move default_snack_method out of class, so it can be used in multiple
      classes
    * Explicitly specify rtol and atol in allclose() and isclose()
    * Fix wrong values used as defaults
    * Test for OpenSauce formant estimates against VoiceSauce estimates
      fails.  See comment.

--- opensauce/snack.py
+++ opensauce/snack.py
@@ -21,7 +21,9 @@ from tools.userconf import user_default_snack_method, user_tcl_shell_cmd
 import logging
 log = logging.getLogger('opensauce.snack')
 
-def snack_pitch(wav_fn, method, frame_length=0.001, window_length=0.0025,
+formant_names = ['sF1', 'sF2', 'sF3', 'sF4', 'sB1', 'sB2', 'sB3', 'sB4']
+
+def snack_pitch(wav_fn, method, frame_length=0.001, window_length=0.025,
                 max_pitch=500, min_pitch=40, tcl_shell_cmd=None):
     """Return F0 and voicing vectors computed from the data in wav_fn.
 
@@ -116,7 +118,6 @@ def snack_pitch_python(wav_fn, frame_length, window_length, max_pitch, min_pitch
     tcl.eval('snack::sound s')
     tcl.eval('s read {}'.format(wav_fn))
     cmd = ['s pitch -method esps']
-    # Let snack use its defaults if no value specified for the keywords.
     cmd.extend(['-framelength {}'.format(frame_length)])
     cmd.extend(['-windowlength {}'.format(window_length)])
     cmd.extend(['-maxpitch {}'.format(max_pitch)])
@@ -205,6 +206,207 @@ def snack_pitch_tcl(wav_fn, frame_length, window_length, max_pitch, min_pitch, t
     os.remove(tcl_file)
 
     return F0, V
+
+def snack_formants(wav_fn, method, frame_length=0.001, window_length=0.025,
+                   pre_emphasis=0.96, lpc_order=12, tcl_shell_cmd=None):
+    """Return formant and bandwidth vectors computed from the data in wav_fn.
+
+    Use Snack to estimate formant and bandwidth for each frame
+
+    windows_length and frame_shift are in seconds. pre_emphasis is used to
+    specify the amount of preemphasis applied to the signal prior to windowing.
+    lpc_order is order for LPC analysis.
+    Note the default parameter values are those used in VoiceSauce.
+
+    method refers to the way in which Snack is called:
+        'exe'    - Call Snack via Windows executable
+        'python' - Call Snack via Python's tkinter Tcl interface
+        'tcl'    - Call Snack via Tcl shell
+
+    tcl_shell_cmd is the name of the command to invoke the Tcl shell.  This
+    argument is only used if method = 'tcl'
+    """
+    if method in valid_snack_methods:
+        if method == 'exe':
+            estimates = snack_formants_exe(wav_fn, frame_length, window_length, pre_emphasis, lpc_order)
+        elif method == 'python':
+            estimates = snack_formants_python(wav_fn, frame_length, window_length, pre_emphasis, lpc_order)
+        elif method == 'tcl':
+            estimates = snack_formants_tcl(wav_fn, frame_length, window_length, pre_emphasis, lpc_order, tcl_shell_cmd)
+    else:
+        raise ValueError('Invalid Snack calling method. Choices are {}'.format(valid_snack_methods))
+
+    return estimates
+
+def snack_formants_exe(wav_fn, frame_length, window_length, pre_emphasis, lpc_order):
+    """Implement snack_formants by calling Snack through a Windows standalone
+       binary executable
+
+       Note this method can only be used on Windows.
+    """
+    # Call Snack using system command to run standalone executable
+    exe_path = os.path.join(os.path.dirname(__file__), 'Windows', 'snack.exe')
+    snack_cmd = [exe_path, 'formant', wav_fn]
+    snack_cmd.extend(['-windowlength', str(window_length)])
+    snack_cmd.extend(['-framelength', str(frame_length)])
+    snack_cmd.extend(['-windowtype', 'Hamming'])
+    snack_cmd.extend(['-lpctype', '0'])
+    snack_cmd.extend(['-preemphasisfactor', str(pre_emphasis)])
+    snack_cmd.extend(['-ds_freq', '10000'])
+    snack_cmd.extend(['-lpcorder', str(lpc_order)])
+    return_code = call(snack_cmd)
+
+    if return_code != 0:
+        raise OSError('snack.exe error')
+
+    # Path for frm file corresponding to wav_fn
+    frm_fn = wav_fn.split('.')[0] + '.frm'
+    # Load data from frm file
+    if os.path.isfile(frm_fn):
+        frm_results = np.loadtxt(frm_fn)
+        # Cleanup and remove frm file
+        os.remove(frm_fn)
+    else:
+        raise OSError('snack.exe error -- unable to locate .frm file')
+
+    num_cols = frm_results.shape[1]
+
+    estimates = {}
+    for i in range(num_cols):
+        estimates[formant_names[i]] = frm_results[:, i]
+
+    return estimates
+
+def snack_formants_python(wav_fn, frame_length, window_length, pre_emphasis, lpc_order):
+    """Implement snack_formants by calling Snack through Python's tkinter library
+
+       Note this method can only be used if the user's machine is setup,
+       so that Tcl/Tk can be accessed through Python's tkinter library
+    """
+    try:
+        import tkinter
+    except ImportError:
+        try:
+            import Tkinter as tkinter
+        except ImportError:
+            print("Need Python library tkinter. Is it installed?")
+
+    # HACK: Need to replace single backslash with two backslashes,
+    #       so that the Tcl shell reads the file path correctly on Windows
+    if sys.platform == 'win32' or sys.platform == 'cygwin':
+        wav_fn = wav_fn.replace('\\', '\\\\')
+
+    # XXX I'm assuming Hz for pitch; the docs don't actually say that.
+    # http://www.speech.kth.se/snack/man/snack2.2/tcl-man.html#spitch
+    tcl = tkinter.Tcl()
+    try:
+        # XXX This will trigger a message 'cannot open /dev/mixer' on the
+        # console if you don't have a /dev/mixer.  You don't *need* a mixer to
+        # snack the way we are using it, but there's no practical way to
+        # suppress the message without modifying the snack source.  Fortunately
+        # most people running opensauce will in fact have a /dev/mixer.
+        tcl.eval('package require snack')
+    except tkinter.TclError as err:
+        log.critical('Cannot load snack (is it installed?): %s', err)
+        return
+    tcl.eval('snack::sound s')
+    tcl.eval('s read {}'.format(wav_fn))
+    cmd = ['s formant']
+    cmd.extend(['-windowlength {}'.format(window_length)])
+    cmd.extend(['-framelength {}'.format(frame_length)])
+    cmd.extend(['-windowtype Hamming'])
+    cmd.extend(['-lpctype 0'])
+    cmd.extend(['-preemphasisfactor {}'.format(pre_emphasis)])
+    cmd.extend(['-ds_freq 10000'])
+    cmd.extend(['-lpcorder {}'.format(lpc_order)])
+    tcl.eval('set data [{}]'.format(' '.join(cmd)))
+    # XXX check for errors here and log and abort if there is one.  Result
+    # string will start with ERROR:.
+    num_frames = int(tcl.eval('llength $data'))
+    num_cols = len(formant_names)
+    estimates = {}
+    for n in formant_names:
+        estimates[n] = np.empty(num_frames)
+    for i in range(num_frames):
+        values = tcl.eval('lindex $data ' + str(i)).split()
+        for j in range(num_cols):
+            estimates[formant_names[j]][i] = np.float_(values[j])
+
+    return estimates
+
+def snack_formants_tcl(wav_fn, frame_length, window_length, pre_emphasis, lpc_order, tcl_shell_cmd):
+    """Implement snack_formants by calling Snack through Tcl shell
+
+    tcl_shell_cmd is the name of the command to invoke the Tcl shell.
+
+       Note this method can only be used if Tcl is installed
+    """
+    # File path for wav file provided to Tcl script
+    in_file = wav_fn
+
+    # ERROR: wind_dur parameter must be between [0.0001, 0.1].
+    # ERROR: frame_step parameter must be between [1/sampling rate, 0.1].
+    # invalid/inconsistent parameters -- exiting.
+
+    # HACK: Tcl shell expects double backslashes in Windows path
+    if sys.platform == 'win32' or sys.platform == 'cygwin':
+        in_file = in_file.replace('\\', '\\\\')
+
+    tcl_file = os.path.join(os.path.dirname(wav_fn), 'tclforsnackformant.tcl')
+
+    # Determine name of system command to invoke Tcl shell
+    if tcl_shell_cmd is not None:
+        tcl_cmd = tcl_shell_cmd
+    elif user_tcl_shell_cmd is not None:
+        tcl_cmd = user_tcl_shell_cmd
+    elif sys.platform == 'darwin':
+        tcl_cmd = 'tclsh8.4'
+    else:
+        tcl_cmd = 'tclsh'
+
+    # Write Tcl script
+    f = open(tcl_file, 'w')
+    script = "#!/usr/bin/env bash\n"
+    script += '# the next line restarts with tclsh \\\n'
+    script += 'exec {} "$0" "$@"\n\n'.format(tcl_cmd)
+    script += 'package require snack\n\n'
+    script += 'snack::sound s\n\n'
+    script += 's read {}\n\n'.format(in_file)
+    script += 'set fd [open [file rootname {}].frm w]\n'.format(in_file)
+    script += 'puts $fd [join [s formant -windowlength {} -framelength {} -windowtype Hamming -lpctype 0 -preemphasisfactor {} -ds_freq 10000 -lpcorder {}]\n\n]\n'.format(window_length, frame_length, pre_emphasis, lpc_order)
+    script += 'close $fd\n\n'
+    script += 'exit'
+    f.write(script)
+    f.close()
+
+    # Run Tcl script
+    try:
+        return_code = call([tcl_cmd, tcl_file])
+    except OSError:
+        os.remove(tcl_file)
+        raise OSError('Error while attempting to call Snack via Tcl shell.  Is Tcl shell command {} correct?'.format(tcl_cmd))
+    else:
+        if return_code != 0:
+            os.remove(tcl_file)
+            raise OSError('Error when trying to call Snack via Tcl shell script.')
+
+    # Load data from f0 file
+    frm_file = os.path.splitext(wav_fn)[0] + '.frm'
+    num_cols = len(formant_names)
+    if os.path.isfile(frm_file):
+        frm_results = np.loadtxt(frm_file).reshape((-1, num_cols))
+        estimates = {}
+        for i in range(num_cols):
+            estimates[formant_names[i]] = frm_results[:, i]
+        # Cleanup and remove f0 file
+        os.remove(frm_file)
+    else:
+        raise OSError('Snack Tcl shell error -- unable to locate .frm file')
+
+    # Cleanup and remove Tcl script file
+    os.remove(tcl_file)
+
+    return estimates
 
 all_functions = inspect.getmembers(sys.modules[__name__], inspect.isfunction)
 prefix_len = len('snack_pitch_')
--- tools/generate_snack_samples.py
+++ tools/generate_snack_samples.py
@@ -3,7 +3,7 @@
 
 import os
 import glob
-from opensauce.snack import snack_pitch
+from opensauce.snack import snack_pitch, snack_formants, formant_names
 from test.support import save_sample_data
 
 def main():
@@ -14,12 +14,26 @@ def main():
     # Generate Snack data for each wav file and save it to text files
     method = 'tcl'
     for wav_file in wav_files:
+        # Generate Snack pitch samples
         # Use VoiceSauce default parameter values
         F0, V = snack_pitch(wav_file, method, frame_length=0.001, window_length=0.025, max_pitch=500, min_pitch=40)
+
+        # Save Snack pitch samples
         wav_basename = os.path.basename(wav_file)
         # Save F0 and V data to separate text files
-        save_sample_data(F0, wav_basename, 'sF0', 'sf0', '1ms')
-        save_sample_data(V, wav_basename, 'V', 'sf0', '1ms')
+        save_sample_data(F0, wav_basename, 'sF0', '1ms')
+        save_sample_data(V, wav_basename, 'sV', '1ms')
+
+        # Generate Snack formant samples
+        # Use VoiceSauce default parameter values
+        estimates = snack_formants(wav_file, method, frame_length=0.001, window_length=0.025, pre_emphasis=0.96, lpc_order=12)
+
+        # Save Snack formant samples
+        wav_basename = os.path.basename(wav_file)
+        # Save F0 and V data to separate text files
+        for n in formant_names:
+            save_sample_data(estimates[n], wav_basename, n, '1ms')
+
 
 if __name__ == '__main__':
     main()
