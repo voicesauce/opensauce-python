597780d4e08435382c4cfa16e8a311c6f561cdf7
Terri Yu <terri@terriyu.info>
2017-06-30 16:46:25 -0400

    Restrict coverage measurement to opensauce directory
    * We're only interested in measuring coverage on the code in the
      opensauce directory, not everything in the entire repo

--- run_coverage.bat
+++ run_coverage.bat
@@ -1,4 +1,4 @@
 REM This is a Windows batch file for running coverage and generating a report
 REM on the core OpenSauce code
-coverage run -m unittest test
-coverage report -m --include=opensauce\*
+coverage run --source=opensauce -m unittest test
+coverage report -m
--- run_coverage.sh
+++ run_coverage.sh
@@ -3,5 +3,5 @@
 # This is a bash script for running coverage and generating a report for
 # the core OpenSauce code.  It should work on both Linux and Mac OS X.
 
-coverage run -m unittest test
-coverage report -m --include=opensauce/*
+coverage run --source=opensauce -m unittest test
+coverage report -m
