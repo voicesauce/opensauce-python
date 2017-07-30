96b2528ec0a554f55bd75a98adb06a49ff812fa4
Terri Yu <terri@terriyu.info>
2017-07-29 22:52:06 -0400

    Add more comparison plots in compare_resample.py
    * Also compare original data to resampling with Hamming window
    * Compare SciPy resampling against Matlab resampling

--- tools/compare_resample.py
+++ tools/compare_resample.py
@@ -16,7 +16,7 @@ from scipy.signal import resample
 # Problems doing this import
 # May need to move this file to top level directory
 from opensauce.helpers import wavread, round_half_away_from_zero
-
+from test.support import load_json
 
 def main(wav_dir, fs_rs):
     """Compare original data vs resampled data for all wav files in wav_dir,
@@ -38,22 +38,49 @@ def main(wav_dir, fs_rs):
         # Resample to 16 kHz
         period_rs = 1.0 / fs_rs
         # Number of points in resample
-        ns_rs = round_half_away_from_zero(ns * fs_rs / fs)
+        ns_rs = np.int_(np.ceil(ns * fs_rs / fs))
         # Do resample
         y_rs, t_rs = resample(y, ns_rs, t)
+        y_h, t_h = resample(y, ns_rs, t, window='hamming')
 
         # Number of points to plot
         n = 1000
         # Start plotting from this data point
+        # Choose random starting point
         s = randint(10000, 35015-n)
+
+        fig = plt.figure()
+        fig.suptitle(os.path.basename(wav_file))
+        # Compare original data with resampled ata
+        ax = plt.subplot(2,1,1)
+        ax.plot(t, y, 'b-', markersize=1)
+        ax.plot(t_rs, y_rs, 'ro', markersize=3)
+        ax.set_xlim([s*period, (s+n)*period])
+        ax.set_ylabel('Amplitude')
+        ax.set_title('Normal resample')
+        # Compare original data with resampling + Hamming window
+        ax = plt.subplot(2,1,2)
+        ax.plot(t, y, 'b-', markersize=1)
+        ax.plot(t_h, y_h, 'ro', markersize=3)
+        ax.set_xlim([s*period, (s+n)*period])
+        ax.set_ylabel('Amplitude')
+        ax.set_xlabel('Time (s)')
+        ax.set_title('Hamming window')
+        plt.savefig(os.path.splitext(os.path.basename(wav_file))[0] + '.pdf')
+
+        fn = os.path.splitext(os.path.basename(wav_file))[0] + '-matlab-resample'
+        data = load_json(os.path.join('soundfile', fn))
+        y_rs_matlab = data['y_rs']
+
+        # Compare SciPy resampled data with Matlab resampled data
         plt.figure()
-        plt.plot(t, y, 'b-', markersize=1)
+        plt.plot(np.linspace(0, period * ns, len(y_rs_matlab)), y_rs_matlab, 'b-', markersize=1)
         plt.plot(t_rs, y_rs, 'ro', markersize=3)
         plt.xlim(s*period, (s+n)*period)
-        plt.title(os.path.basename(wav_file))
+        plt.title(os.path.basename(wav_file) + ' - Matlab comparison')
         plt.xlabel('Time (s)')
         plt.ylabel('Amplitude')
-        plt.savefig(os.path.splitext(os.path.basename(wav_file))[0] + '.pdf')
+        plt.savefig(os.path.splitext(os.path.basename(wav_file))[0] + '-matlab.pdf')
 
 if __name__ == '__main__':
     main(sys.argv[1], int(sys.argv[2]))
